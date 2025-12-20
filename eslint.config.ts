18675ff5c561f8caa5d7736b1da296dbe5278794
Abdulloh-dev03 <abdullohortqiov15@gmail.com>
2025-12-20 21:18:42 +0500

    Implement Eslint and Prettier

---
+++ eslint.config.ts
@@ -0,0 +1,65 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettierConfig from 'eslint-config-prettier';
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettierConfig,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+        Buffer: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+        URL: 'readonly',
+        setTimeout: 'readonly',
+        clearTimeout: 'readonly',
+        setInterval: 'readonly',
+        clearInterval: 'readonly',
+      },
+    },
+    rules: {
+      'linebreak-style': ['error', 'unix'],
+      quotes: ['error', 'single', { avoidEscape: true }],
+      semi: ['error', 'always'],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_' },
+      ],
+      'no-console': 'off',
+      'prefer-const': 'error',
+      'no-var': 'error',
+      'object-shorthand': 'error',
+      'prefer-arrow-callback': 'error',
+    },
+  },
+  {
+    files: ['tests/**/*.ts', 'tests/**/*.js'],
+    languageOptions: {
+      globals: {
+        describe: 'readonly',
+        it: 'readonly',
+        expect: 'readonly',
+        beforeEach: 'readonly',
+        afterEach: 'readonly',
+        beforeAll: 'readonly',
+        afterAll: 'readonly',
+        jest: 'readonly',
+      },
+    },
+  },
+  {
+    ignores: [
+      'node_modules/**',
+      'dist/**',
+      'coverage/**',
+      'logs/**',
+      'drizzle/**',
+    ],
+  }
+);
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,9 @@
-import express, { Request, Response } from "express";
-
+import express, { Request, Response } from 'express';
 
 const app = express();
 
-
-app.get("/", (_req: Request, res: Response) => {
-  res.status(200).send("Hello from Acquisitions!");
+app.get('/', (_req: Request, res: Response) => {
+  res.status(200).send('Hello from Acquisitions!');
 });
 
 export default app;
